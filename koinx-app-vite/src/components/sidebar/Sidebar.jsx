cfd2a1b2c1a031e051dd8e1f8382cbad6826994c
musaib88 <musaibbashir2022@gmail.com>
2024-03-07 18:46:50 +0530

    sidebarUpdate

--- koinx-app-vite/src/components/sidebar/Sidebar.jsx
+++ koinx-app-vite/src/components/sidebar/Sidebar.jsx
@@ -3,8 +3,27 @@ import './sidebar.css'
 import bannerImg from '../../util/bannerAd.png'
 import coinImg from '../../util/Bitcoin.svg.png'
 export default function Sidebar({coins}) {
-// const top3=[coins[0],coins[1],coins[2]]
-console.log(coins)
+ const top3 = coins && coins.length > 0 ? coins.slice(0, 3) : [];
+  const style = {
+    priceUp: { backgroundColor: "#EBF9F4", color: "green" },
+    priceDown: { backgroundColor: "#f4e5e1", color: "red" },
+  };
+
+ const mappedData = top3.map((coin) => {
+  const { name, symbol, data ,thumb} = coin.item;
+  const priceChangePercentage24h = data.price_change_percentage_24h;
+  
+
+  return {
+    name,
+    symbol,
+    priceChangePercentage24h,
+    thumb
+  };
+});
+console.log(mappedData,'jjj')
+
+ console.log(top3)
   return (
     <div id='sidebar-layout'>
       <div id='koinx-banner'>
@@ -23,28 +42,26 @@ console.log(coins)
         <button id='banner-button'>Get Started for Free <i class="fa-solid fa-arrow-right"></i></button>
       </div>
 
+
+      
       <div id='trending-3-coins-sidebar'>
           <div id='trending-3-top-title'><span>Trending Coins (24h)</span></div>
-           <div id='trending-top3-coins-show'>
-              <div id='image-layout-top3-coins'> <img src={coinImg} alt="" /></div>
-             <span id='coin-name-top3'> Ethererum(ETH)</span>
-             <span id='top3-coin-perf-24'> <i className="fa-solid fa-caret-up"></i>5.6%</span>
-             
+        {
+          mappedData.map((coin,index)=>(
+            <div key={index} id='trending-top3-coins-show'>
+              <div style={{display:'flex', alignItems:'center',justifyContent:'center',gap:'0.5rem'}}><div id='image-layout-top3-coins'> <img src={coin.thumb} alt="" /></div>
+             <span id='coin-name-top3'> {coin.name}(coin.symbol)</span>
+             </div>
+             <span id='top3-coin-perf-24' style={ coin.priceChangePercentage24h.usd>0?style.priceUp:style.priceDown}> {coin.priceChangePercentage24h.usd >=0 ? <i className="fa-solid fa-caret-up"></i>: <i className="fa-solid fa-caret-down"></i>} {`${Math.abs(coin.priceChangePercentage24h.usd).toFixed(2)}%`}</span>
 
+               </div>
+          ))
+        }
 
+          
+          
 
-           </div>
-
-           <div id='trending-top3-coins-show'>
-              <div id='image-layout-top3-coins'> <img src={coinImg} alt="" /></div>
-             <span id='coin-name-top3'> Ethererum (ETH)</span>
-             <span id='top3-coin-perf-24'> <i className="fa-solid fa-caret-up"></i>5.6%</span>
-             
-
-
-
-           </div>
-           
+          
            
        
       </div>
